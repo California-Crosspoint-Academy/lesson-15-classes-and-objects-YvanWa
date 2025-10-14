02068c83c292850cf896278ac0cd04601d888964
YvanWa <yvanwang26@crosspointacademy.org>
2025-10-14 03:07:09 +0000

    commit

---
+++ circle.java
@@ -0,0 +1,14 @@
+public class circle{
+        public circle(double r)
+        {
+            r1 = r;
+        }
+        public double area()
+        {
+            double a= Math.PI*r1*r1;
+            return a;
+        }
+        public double r1;
+}
+
+
---
+++ exercise.java
@@ -0,0 +1,91 @@
+
+public class exercise{
+    public static void main(String[]args){
+
+
+
+        circle cir1= new circle(5.1);
+        circle cir2= new circle(20.6);
+        System.out.println(cir2.area());
+        System.out.println(cir1.area());
+
+    //1
+    //class: Rectangle
+    //object : myRect
+    //double (length)  and int (width)
+
+    //2 
+    //public Rectangle(double lenghth, int width)
+
+    //3
+    // Lunch yummy5= new Lunch(true, 900);
+
+    //4
+    //class: BankAccount
+    //object: account39
+    //double
+
+    //5
+    //A class is like a cookie cutter. An objsct is like cookie.
+    
+    //6
+    // consturctor name should be School
+    //void should not exist
+
+    //7  a
+    
+    //8  a
+
+    //9 no. The consturctor for House assigned parameters in an 'int' and a 'boolean'. but the tester code has both parameters in int. parameter type mismatch
+    
+    //10    
+    //a int
+    //b int
+    //c double
+
+    //11 
+    //public BibleStory(String x, int y, double z);
+    
+    //12
+    // var1, var2, sss
+    
+    //13
+    //public void Samson(double)
+    //public String getDelilah()
+
+    //14
+    //BibleStory philistine = new BibleStory("Ralph", 19, 24.18);
+
+    //15
+    // gravy.var2= 106.9
+    
+    //16
+    //System.out.println(bart.sss);
+
+    //17
+    // String
+
+    //18
+    //public class Trail{
+    //   public int x;
+    //   public int y;
+    //public String s;
+
+    //public Trail(String intitialS){
+    //    s= initialS;
+    //     x=10;
+    //      y=10;
+    //}
+    //public String mer(）{
+    //      int product= x*y;
+    // return Integer.toHexString(product);
+    //}
+    //}
+
+    //19
+    //double result = zippo.peachyDandy(5);
+
+    //20
+    //The method expects an int argument, but it was given a double (127.31).
+    }
+}
