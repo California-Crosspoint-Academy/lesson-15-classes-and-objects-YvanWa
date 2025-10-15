c7418480b5edf0054a44bf2eb064535fe5116bbe
YvanWa <yvanwang26@crosspointacademy.org>
2025-10-15 09:42:13 -0700

    commit2

--- circle.java
+++ circle.java
@@ -3,13 +3,16 @@ public class circle{
         {
             r1 = r;
         }
+        public double diameter(){
+            return 2*r1;
+        }
         public double area()
         {
             double a= Math.PI*r1*r1;
             return a;
         }
         public double circumference(){
-            return 2*Math.PI*radius;
+            return 2*Math.PI*r1;
         }
         public double r1;
 
--- lab.java
+++ lab.java
@@ -1,5 +1,10 @@
 public class lab{
     public static void main(String[] args){
-        
+        circle cir1= new circle(5.1);
+        circle cir2= new circle(20.6);
+        System.out.println(cir2.area());
+        System.out.println(cir1.area());
+        System.out.println(cir2.diameter());
+        System.out.println(cir1.diameter());
     }
 }
